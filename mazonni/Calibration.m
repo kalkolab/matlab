4ff3de04682ef2011f00af6132f84df8ffab1ea0
kalkolab <artem.kalchenko@gmail.com>
2016-06-02 09:09:40 +0300

    duan

--- mazonni/Calibration.m
+++ mazonni/Calibration.m
@@ -1,61 +1,50 @@
-% Heston Model Calibration
-%
-% Options2011: set of options with on index
-% 
-% Author: Jonathan Frei, 2016
-%
-
-
-load('Options2011.mat');
-% K is a vector of strikes 
-% PC is a vector containing either a 1 or 2 (1 for call, 2 for put)
-% Price is a vector of market prices of the options 
-% S is a vector of spot prices
-% T is a vector of maturities (must be the same unit as r and q)
-% dateO is the starting date of the option
-% r is a vector of interest rates
-% q is a vector of dividend rates
-
-
+%% load
+data = dlmread('sample.csv', ';', 1, 0);
 
 % PC==1 for calls, PC==2 is for puts
 % dateO==min(dateO) selects the first dates only (Wednesdays here)
-indx = find(dateO==min(dateO)&PC==1); 
 
 % reducing vectors to selected options
-K = K(indx);
-S = S(indx);
-T = T(indx);
+K = data(:,3);
+S = data(:,2);
+T = data(:,4);
 t = zeros(size(T)); 
-r = r(indx);
-q = q(indx);
-PC = PC(indx);
-MarketPrice = Price(indx);
+r = data(:,6);
+q = zeros(size(r));
+PC = ones(size(S));
+MarketPrice = data(:,1);
 
 
+%% test
+%mazzoniCall(S,K,T,t,r-q, 1e-6,.5e-5, 0.6, 100, 0.04)
+%% marketIV
 MarketIVCheck = blsimpv(S, K, r, T, MarketPrice, [], q, [], (PC==1))*100;
 [ MarketIV, dates ] = bsmivec( MarketPrice, S,K,T,t,r, q, PC );
 
-
+%% calibrate
 
 tic
 
-startparameters = [2.5e-4 1e-3 0.4 10 0.04 2e-4];
-options = optimoptions('lsqnonlin', 'Display', 'iter');
-xopt = lsqnonlin(@(x) mazzoniCall(S,K,T,t,r-q,x(1),x(2),x(3),x(4),x(5),x(6)) - MarketPrice,...
+startparameters = [1e-6 .5e-5 0.6 100 0.04 ];
+options = optimoptions('lsqnonlin', 'Display', 'iter', 'MaxFunEvals', Inf, 'MaxIter', Inf, 'TolFun', 1e-6);
+xopt = lsqnonlin(@(x) mazzoniCall(S,K,T,t,r-q,x(1),x(2),x(3),x(4),x(5)) - MarketPrice,...
     startparameters, ... %start values
     ... % v0,theta,rho,kappa,sigma
-    [eps eps -1+eps eps eps eps ], ... % lower bound for parameter vector
-    [Inf Inf 1-eps Inf  Inf Inf ], ...  % upper bound for parameter vector
+    [eps eps eps eps eps ], ... % lower bound for parameter vector
+    [Inf Inf Inf  Inf Inf ], ...  % upper bound for parameter vector
     options);
 
+% options = optimoptions('fminsearch', 'Display', 'iter', 'MaxFunEvals', Inf, 'MaxIter', Inf, 'TolFun', 1e-6);
+% xopt = fminsearch(@(x) sum((mazzoniCall(S,K,T,t,r-q,x(1),x(2),x(3),x(4),x(5)) - MarketPrice).^2),...
+%     startparameters, ... %start values
+%     options);
 
 toc
 
 
-
+%% show res
 disp(['Optimal parameter vector: ' num2str(xopt)]);
-ModelPrices =  mazzoniCall(S,K,T,t,r-q,xopt(1),xopt(2),xopt(3),xopt(4),xopt(5),xopt(5))
+ModelPrices =  mazzoniCall(S,K,T,t,r-q,xopt(1),xopt(2),xopt(3),xopt(4),xopt(5))
 %ModelIVCheck = blsimpv(S, K, r, T, ModelPrices, [], q, [], (PC==1));
 ModelIV = bsmivec( ModelPrices, S,K,T,t,r, q, PC );
 disp(['RMSE: ' num2str(sqrt(mean((ModelPrices-MarketPrice).^2)))]);
--- mazonni/mazzoniCall.m
+++ mazonni/mazzoniCall.m
@@ -1,4 +1,4 @@
-function [ prices ] = mazzoniCall( S, K, T, t, mu, w, alpha, beta, gamma, sigma, r )
+function [ prices ] = mazzoniCall( S, K, T, t, r, w, alpha, beta, gamma, sigma)
 %MAZONNI Summary of this function goes here
 %   Detailed explanation goes here
     % force column vector
@@ -6,17 +6,23 @@ function [ prices ] = mazzoniCall( S, K, T, t, mu, w, alpha, beta, gamma, sigma,
     K=K(:);
     T=T(:);
     t=t(:);
-    mu=mu(:);
+    r=r(:);
 
     nos = numel(S);
     prices=NaN(nos,1);
     tau=T-t;
     
-    F = S.*exp(mu.*tau);
-
-    h = var(S);
+    if (alpha < 0 | w <0 | beta <0 | gamma <0 | sigma <0) 
+        prices = Inf(nos,1);
+        return
+    end
+    
+%    h = var(log(S(2:numel(S))./S(1:numel(S)-1)));
+    h = 0.05^2;
     
     rho = alpha*gamma^2 + beta;
+    
+    mu = tau.*r - (h*(rho.^tau - 1))/(2*(rho - 1)) + ((tau/2 - (rho.^tau - 1)/(2*(rho - 1)))*(alpha + w))/(rho - 1);
     
     d2 = (log(S./K) + mu)./sigma;
     d1 = d2 + sigma;
@@ -29,7 +35,7 @@ function [ prices ] = mazzoniCall( S, K, T, t, mu, w, alpha, beta, gamma, sigma,
     k4 = ((alpha + w).*((48.*alpha.^2.*gamma.*(rho.^tau - 1))./(rho - 1).^3 - (12.*alpha.*(rho.^tau - 1))./(rho - 1).^2 + (48.*alpha.^2.*gamma.^2.*(rho.^tau - 1))./(rho - 1).^3 - (96.*alpha.^3.*gamma.^3.*(rho.^tau - 1))./(rho - 1).^4 + (12.*tau.*alpha.*rho.^(tau - 1))./(rho - 1) - (48.*tau.*alpha.^2.*gamma.*rho.^(tau - 1))./(rho - 1).^2 - (48.*tau.*alpha.^2.*gamma.^2.*rho.^(tau - 1))./(rho - 1).^2 + (96.*tau.*alpha.^3.*gamma.^3.*rho.^(tau - 1))./(rho - 1).^3 + (24.*tau.*alpha.^2.*gamma.*rho.^(tau - 2).*(tau - 1))./(rho - 1) + (24.*tau.*alpha.^2.*gamma.^2.*rho.^(tau - 2).*(tau - 1))./(rho - 1) - (48.*tau.*alpha.^3.*gamma.^3.*rho.^(tau - 2).*(tau - 1))./(rho - 1).^2 + (16.*tau.*alpha.^3.*gamma.^3.*rho.^(tau - 3).*(tau - 1).*(tau - 2))./(rho - 1)))./(rho - 1) + (12.*alpha.*(alpha + w).*(tau - (rho.^tau - 1)./(rho - 1) + (2.*alpha.*gamma.*(rho.^tau - 1))./(rho - 1).^2 - (2.*tau.*alpha.*gamma.*rho.^(tau - 1))./(rho - 1)))./(rho - 1).^2 - (12.*alpha.*h.*(rho.^tau - 1))./(rho - 1).^2 + (48.*alpha.^2.*gamma.^2.*h.*(rho.^tau - 1))./(rho - 1).^3 - (96.*alpha.^3.*gamma.^3.*h.*(rho.^tau - 1))./(rho - 1).^4 - (96.*alpha.^2.*gamma.*(tau./2 - (rho.^tau - 1)./(2.*(rho - 1))).*(alpha + w))./(rho - 1).^3 - (48.*alpha.^2.*gamma.^2.*(alpha + w).*(tau - (rho.^tau - 1)./(rho - 1) + (2.*alpha.*gamma.*(rho.^tau - 1))./(rho - 1).^2 - (2.*tau.*alpha.*gamma.*rho.^(tau - 1))./(rho - 1)))./(rho - 1).^3 + (12.*tau.*alpha.*h.*rho.^(tau - 1))./(rho - 1) + (192.*alpha.^3.*gamma.^3.*(tau./2 - (rho.^tau - 1)./(2.*(rho - 1))).*(alpha + w))./(rho - 1).^4 - (8.*alpha.*gamma.*(alpha + w).*((12.*alpha.^2.*gamma.^2.*(rho.^tau - 1))./(rho - 1).^3 - (3.*alpha.*(rho.^tau - 1))./(rho - 1).^2 + (3.*tau.*alpha.*rho.^(tau - 1))./(rho - 1) - (6.*alpha.*gamma.*(rho.^tau - 1))./(rho - 1).^2 + (6.*tau.*alpha.*gamma.*rho.^(tau - 1))./(rho - 1) - (12.*tau.*alpha.^2.*gamma.^2.*rho.^(tau - 1))./(rho - 1).^2 + (6.*tau.*alpha.^2.*gamma.^2.*rho.^(tau - 2).*(tau - 1))./(rho - 1)))./(rho - 1).^2 + (48.*alpha.^2.*gamma.*h.*(rho.^tau - 1))./(rho - 1).^3 - (48.*tau.*alpha.^2.*gamma.^2.*h.*rho.^(tau - 1))./(rho - 1).^2 + (96.*tau.*alpha.^3.*gamma.^3.*h.*rho.^(tau - 1))./(rho - 1).^3 - (48.*tau.*alpha.^2.*gamma.*h.*rho.^(tau - 1))./(rho - 1).^2 + (24.*tau.*alpha.^2.*gamma.^2.*h.*rho.^(tau - 2).*(tau - 1))./(rho - 1) - (48.*tau.*alpha.^3.*gamma.^3.*h.*rho.^(tau - 2).*(tau - 1))./(rho - 1).^2 + (24.*tau.*alpha.^2.*gamma.*h.*rho.^(tau - 2).*(tau - 1))./(rho - 1) + (16.*tau.*alpha.^3.*gamma.^3.*h.*rho.^(tau - 3).*(tau - 1).*(tau - 2))./(rho - 1);
     c4 = k4/24 .* (d1.^2-1-3*sigma*d2)/sigma^3;
     
-    prices = S.*normcdf(d1) - exp(-r.*tau).*K.*normcdf(d2) + S.*normpdf(d1).*exp(mu+sigma^2/2 - r*tau).*(c3+c4);
+    prices = S.*normcdf(d1) - exp(-r.*tau).*K.*normcdf(d2) + S.*normpdf(d1).*exp(mu+sigma^2/2 - r.*tau).*(c3+c4);
     
 %     for(ind=1:nos)
 %         prices(ind) =  Ralpha(F(ind), K(ind), alphas(ind))+1/pi*integral(@(x) phi(x, K(ind), alphas(ind), F(ind), kappa, theta, rho, sigma, tau(ind), v0) , 0, Inf);
